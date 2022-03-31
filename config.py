1644065616ac2f6f71a78034668004496a740c04
sizin github kullanıcı adı <you@example.com>
2022-03-31 14:33:16 +0300

    first commit

---
+++ config.py
@@ -0,0 +1,10 @@
+# SETTINGS and CONSTANTS    # Examples
+# [JIRA]
+JIRA_URL =                  # "http://44.202.209.23:8080"
+JIRA_USER =                 # "admin"
+JIRA_PASS =                 # "Python01*"
+
+# [BITBUCKET]           # Examples
+BB_API_URL =                # "http://44.202.209.23:7990"
+BB_USER =                   # "admin"
+BB_PASS =                   # "Python01*"
